c1c3edfca74d386e6697bc53b7503571ad917023
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-11-21 18:21:46 +0530

    Update newSort.c

--- Data Structure Through C/3_Sorting/newSort.c
+++ Data Structure Through C/3_Sorting/newSort.c
@@ -1,13 +1,100 @@
 #include <stdio.h>
 
 void showArray(int arr[], int n) {
+    printf("Elements of the array : ");
     for(int i=0; i<n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
-void partition(int arr[], int start, int end, int *loc) {
-    int L=start, R=end, done=0, temp;
+/*
+    COMPLEXITY ANALYSIS OF THE ALGORITHM (Quick Sorting of Arrays)
+
+    Time Complexity : 
+        Worst Case - O(n^2)
+        Average Case - O(nlogn)
+        Best Case - O(nlogn)
+    Space Complexity - O(n)
+    
+    ---------------------------------
+    
+    VARIABLES USED IN THE FUNCTION : 
+
+    arr - The given array
+    size - Size of the array
+    start - First index of the array
+    end - Last index of the array
+    p - Index of partition element
+
+    temp - Temporary variable used for swaping elements
+    pivot - Pivot index
+    i, j - Loop counters
+*/
+int partition(int arr[], int start, int end) {
+    int pivot = arr[end];
+    int i = start - 1;
+    
+    for(int j=start; j<=end-1; j++) {
+        if(arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i+1];
+    arr[i+1] = arr[end];
+    arr[end] = temp;
+
+    return i+1;
+}
+
+void quickSort(int arr[], int start, int end) {
+    if(start >= end) {
+        return;
+    }
+
+    int p = partition(arr, start, end);
+
+    quickSort(arr, start, p-1);
+    quickSort(arr, p+1, end);
+}
+
+int main() {
+    int arr[10] = {7, 9, 15, 49, -87, 0, 21, 54, 16, 76};
+
+    showArray(arr, 10);
+
+    quickSort(arr, 0, 9);
+
+    showArray(arr, 10);
+
+    return 0;
+}
+
+/*
+    Another logic for quick sort explained by Mam (Anjna Chaudhary)
+    
+    ---------------------------------
+    
+    VARIABLES USED IN THE FUNCTION : 
+
+    arr - The given array
+    lb - Lower bound of the array
+    ub - Upper bound of the array
+    loc - Index of pivot element
+
+    temp - Temporary variable used for swaping elements
+    L - Left most index of working array
+    R - Right most index of working array
+    done - Flag variable to check the status of the function
+*/
+
+/*
+
+void partition(int arr[], int lb, int ub, int *loc) {
+    int L=lb, R=ub, done=0, temp;
     *loc = L;
 
     while(!done) {
@@ -41,132 +128,14 @@ void partition(int arr[], int start, int end, int *loc) {
     }
 }
 
-void quickSort(int arr[], int start, int end) {
-    int *loc;
+void quickSort(int arr[], int lb, int ub) {
+    int loc;
 
-    if(start < end) {
-        partition(arr, start, end, &loc);
+    if(lb < ub) {
+        partition(arr, lb, ub, &loc);
         
-        quickSort(arr, start, loc-1);
-        quickSort(arr, loc+1, end);
-    }
-}
-
-
-int main() {
-    int arr[10] = {7, 9, 15, 49, -87, 0, 21, 54, 16, 76};
-
-    printf("Elements of the array before sorting : ");
-    showArray(arr, 10);
-
-    quickSort(arr, 0, 9);
-
-    printf("Elements of the array after sorting : ");
-    showArray(arr, 10);
-
-    return 0;
-}
-
-// Making the code right by bruteforce type casting
-/*
-
-void partition(int arr[], int start, int end, int *loc) {
-    int L=start, R=end, done=0, temp;
-    loc = (int*) L;
-
-    while(!done) {
-        while(arr[(int) loc]<=arr[R] && (int) loc!=R) {
-            R--;
-        }
-
-        if(R == (int) loc) {
-            done = 1;
-        } else if(arr[(int) loc] > arr[R]) {
-            temp = arr[(int) loc];
-            arr[(int) loc] = arr[R];
-            arr[R] = temp;
-            loc = (int*) R;
-        }
-
-        if(!done) {
-            while(arr[(int) loc]>=arr[L] && (int) loc!=L) {
-                L++;
-            }
-
-            if(L == (int) loc) {
-                done = 1;
-            } else if(arr[(int) loc] < arr[L]) {
-                temp = arr[(int) loc];
-                arr[(int) loc] = arr[L];
-                arr[L] = temp;
-                loc = (int*) L;
-            }
-        }
-    }
-}
-
-void quickSort(int arr[], int start, int end) {
-    int *loc;
-
-    if(start < end) {
-        partition(arr, start, end, loc);
-        
-        quickSort(arr, start, (int) loc-1);
-        quickSort(arr, (int) loc+1, end);
-    }
-}
-
-*/
-
-
-
-
-// Right code with integer return type
-/*
-
-int partition(int arr[], int start, int end) {
-    int L=start, R=end, done=0, temp;
-    int loc = L;
-
-    while(!done) {
-        while(arr[loc]<=arr[R] && loc!=R) {
-            R--;
-        }
-
-        if(R == loc) {
-            done = 1;
-        } else if(arr[loc] > arr[R]) {
-            temp = arr[loc];
-            arr[loc] = arr[R];
-            arr[R] = temp;
-            loc = R;
-        }
-
-        if(!done) {
-            while(arr[loc]>=arr[L] && loc!=L) {
-                L++;
-            }
-
-            if(L == loc) {
-                done = 1;
-            } else if(arr[loc] < arr[L]) {
-                temp = arr[loc];
-                arr[loc] = arr[L];
-                arr[L] = temp;
-                loc = L;
-            }
-        }
-    }
-
-    return loc;
-}
-
-void quickSort(int arr[], int start, int end) {
-    if(start < end) {
-        int loc = partition(arr, start, end);
-
-        quickSort(arr, start, loc-1);
-        quickSort(arr, loc+1, end);
+        quickSort(arr, lb, loc-1);
+        quickSort(arr, loc+1, ub);
     }
 }
 
