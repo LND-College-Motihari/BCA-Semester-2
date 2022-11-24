bea3feff5b2355213171357d12ccd4941865ffa8
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2022-11-24 07:56:22 +0530

    Add files via upload

---
+++ Data Structure Through C/7_Tree/2_PreOrder_Traversal.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    PreOrder (Prefix Order) Traversal of binary tree
+    Algorithm used : NLR (Node - Left - Right)
+    
+    ---------------------------------
+
+    VARIABLES USED IN THE PROGRAM :
+    
+    root : Root node of the tree
+    Node->data : Stores the data of that particular node
+    Node->left : Stores the address of left child of that particular node
+    Node->right : Stores the address of right child of that particular node
+
+    stack : An array based stack type (LIFO) structure to store address of nodes
+    top : Index of top element of the stack
+
+    track : A temporary variable used for traversing through the tree
+*/
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Tree;
+
+Tree *root = NULL;
+
+Tree* buildTree(Tree *root) {
+    int value;
+    printf("Enter the value (-1 for NULL) : ");
+    scanf("%d", &value);
+
+    if(value == -1) {
+        return NULL;
+    }
+
+    root = (Tree*) malloc(sizeof(Tree*));
+    root->data = value;
+    root->left = NULL;
+    root->right = NULL;
+
+    if(value == -1) {
+        return NULL;
+    }
+
+    printf("Left child of %d - ", value);
+    root->left = buildTree(root->left);
+
+    printf("Right child of %d - ", value);
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+void preOrderIterative() {
+    Tree *stack[20];
+    int top = -1;
+    stack[++top] = root;
+
+    Tree *track = root;
+
+    while(top >= 0) {
+        track = stack[top--];
+
+        if(track != NULL) {
+            printf("%d ", track->data);
+            
+            if(track->right != NULL) {
+                stack[++top] = track->right;
+            }
+
+            if(track->left != NULL) {
+                stack[++top] = track->left;
+            }
+        }
+    }
+}
+
+void preOrderRecurive(Tree *root) {
+    if(root == NULL) {
+        return;
+    }
+
+    printf("%d ", root->data);
+    preOrderRecurive(root->left);
+    preOrderRecurive(root->right);
+}
+
+int main() {
+    // Input : 10 15 20 -1 -1 25 -1 -1 30 40 -1 -1 50 -1 -1
+    root = buildTree(root);
+
+    printf("\n\n\nElements of tree in PreOrder Traversal (Iterative method) : ");
+    preOrderIterative();
+
+    printf("\nElements of tree in PreOrder Traversal (Recursive method) : ");
+    preOrderRecurive(root);
+
+    return 0;
+}
