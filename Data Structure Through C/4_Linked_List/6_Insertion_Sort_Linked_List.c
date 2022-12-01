3db41d78019bf4df4d8e2b19310d8e66c414a00b
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2022-12-01 23:26:04 +0530

    Update and rename 6_Insertion_Sort.c to 6_Insertion_Sort_Linked_List.c

--- Data Structure Through C/4_Linked_List/6_Insertion_Sort_Linked_List.c
+++ Data Structure Through C/4_Linked_List/6_Insertion_Sort_Linked_List.c
@@ -32,31 +32,49 @@ void insertAtHead(int element) {
     head = newNode;
 }
 
-void insertionSort() {
-    SLL *curr = head;
-    SLL *sorted = NULL;
+/*
+    COMPLEXITY ANALYSIS OF THE ALGORITHM (Insertion Sorting of Linked Lists)
+
+    Time Complexity - O(n^2)
+    Space Complexity - O(1)
     
-    while(curr != head) {
+    ---------------------------------
+    
+    VARIABLES USED IN THE FUNCTION : 
+
+    haed - Pointer variable pointing at the first element of the given linked list
+    head->data - Data memeber of the list
+    head->next - Pointer variable of the list
+    
+    sorted - Node pointer used to store the first node of the sorted list
+    curr - Node pointer used to store the current element for insertion
+    next - Node pointer containing address of the next node
+    temp - Node variable used to traverse through the sorted list to find the right position for curr
+*/
+void insertionSort() {
+    SLL *sorted = NULL;
+    SLL *curr = head;
+
+    while(curr != NULL) {
         SLL *next = curr->next;
-        
+
         if(sorted == NULL || sorted->data >= curr->data) {
             curr->next = sorted;
             sorted = curr;
         } else {
             SLL *temp = sorted;
-            
+
             while(temp->next != NULL && temp->next->data < curr->data) {
                 temp = temp->next;
             }
-            
+
             curr->next = temp->next;
             temp->next = curr;
         }
-        
-        curr->next = temp->next;
-        temp->next = curr;
+
+        curr = next;
     }
-    
+
     head = sorted;
 }
 
